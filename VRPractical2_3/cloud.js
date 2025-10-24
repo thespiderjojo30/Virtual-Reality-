84afe29cd07e06504857183646aab215c5e9f324
thespiderjojo30 <josephsamuelc@nycstudents.net>
2025-10-24 02:44:14 +0000

    sadsad

--- VRPractical2_3/cloud.js
+++ VRPractical2_3/cloud.js
@@ -8,13 +8,13 @@ class Clouds{
     
     let clouds = document.createElement("a-sphere");
     clouds.setAttribute("color","white");
-    clouds.setAttribute("position","0 0 0");
-    clouds.setAttribute("radius","1");
+    clouds.setAttribute("position","1 0.3 0");
+    clouds.setAttribute("radius","0.8");
     this.obj.append(clouds);
 
    let cloud1 = document.createElement("a-sphere");
     cloud1.setAttribute("color","white");
-    cloud1.setAttribute("position","0 0 0");
+    cloud1.setAttribute("position","1 5 -0.5");
     cloud1.setAttribute("radius","1");
     this.obj.append( cloud1 );
 
@@ -27,7 +27,7 @@ class Clouds{
     let cloud3 = document.createElement("a-sphere");
     cloud3.setAttribute("color","white");
     cloud3.setAttribute("position","-1 0.5 0");
-    cloud3.setAttribute("radius","1");
+    cloud3.setAttribute("radius","0.5");
     this.obj.append( cloud3 );
 
    scene.append(this.obj);
